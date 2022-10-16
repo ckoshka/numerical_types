177c918a5e600fbfc9bf09a83f14d3e863dc46ba
fastelectronicvegetable <48640397+ckoshka@users.noreply.github.com>
2022-10-17 11:54:50 +1300

    + validator fns

--- mod.ts
+++ mod.ts
@@ -2,3 +2,6 @@ export type int = number & { __int: never };
 export type unitFloat = number & { __unitFloat: never };
 export type float = number & { __float: never };
  
+export const isInt = (n: number): n is int => Math.round(n) === n;
+export const isUnitFloat = (n: number): n is unitFloat => n < 1.0 && n > 0.0;
+export const isFloat = (n: number): n is float => !isInt(n);
