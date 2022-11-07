d7a6e59d0d390b9a4b959562977e321640e9ddf7
fastelectronicvegetable <48640397+ckoshka@users.noreply.github.com>
2022-11-08 09:01:42 +1300

    breaking: test NaN

--- mod.ts
+++ mod.ts
@@ -2,9 +2,9 @@ export type int = number & { __int: never };
 export type unitFloat = number & { __unitFloat: never };
 export type float = number & { __float: never };
  
-export const isInt = (n: number): n is int => Math.round(n) === n;
+export const isInt = (n: number): n is int => Math.round(n) === n && !isNaN(n);
 export const isUnitFloat = (n: number): n is unitFloat => n < 1.0 && n > 0.0;
-export const isFloat = (n: number): n is float => !isInt(n);
+export const isFloat = (n: number): n is float => !isInt(n) && !isNaN(n);
 
 export const Int = (i: number) => {
     if (isInt(i)) {
